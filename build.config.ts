07016e53e7d72a6c63feaebc741b205339075613
Lexus Drumgold <unicornware@flexdevelopment.llc>
2023-03-12 17:32:19 -0400

    build(cjs): default export `module.exports.default`
    - babel/babel#2212
    - https://esbuild.github.io/content-types/#default-interop
    
    Signed-off-by: Lexus Drumgold <unicornware@flexdevelopment.llc>

--- build.config.ts
+++ build.config.ts
@@ -5,6 +5,8 @@
  */
 
 import { defineBuildConfig, type Config } from '@flex-development/mkbuild'
+import type { BuildResult, OutputFile, PluginBuild } from 'esbuild'
+import util from 'node:util'
 import pkg from './package.json' assert { type: 'json' }
 
 /**
@@ -14,7 +16,61 @@ import pkg from './package.json' assert { type: 'json' }
  */
 const config: Config = defineBuildConfig({
   conditions: ['import', 'require', 'node', 'default'],
-  entries: [{}, { format: 'cjs' }],
+  entries: [
+    {},
+    {
+      format: 'cjs',
+      plugins: [
+        {
+          name: 'add-module-exports',
+          setup({ initialOptions, onEnd }: PluginBuild): void {
+            const { format } = initialOptions
+
+            // do nothing if format is not commonjs
+            if (format !== 'cjs') return void format
+
+            // add module exports
+            return void onEnd(
+              (result: BuildResult<{ metafile: true; write: false }>): void => {
+                /**
+                 * Output file objects.
+                 *
+                 * @const {OutputFile[]} outputFiles
+                 */
+                const outputFiles: OutputFile[] = result.outputFiles
+
+                return void (result.outputFiles = outputFiles.map(output => {
+                  if (/dist\/(?:index|ponyfill)\.cjs$/.test(output.path)) {
+                    // get sourceMappingURL comment
+                    const [sourcemap = ''] = /\/\/#.+\n/.exec(output.text) ?? []
+
+                    /**
+                     * Output file content.
+                     *
+                     * @const {string} text
+                     */
+                    const text: string =
+                      output.text.replace(sourcemap, '') +
+                      'module.exports = module.exports.default;\n' +
+                      'module.exports.default = module.exports;\n' +
+                      sourcemap
+
+                    return {
+                      ...output,
+                      contents: new util.TextEncoder().encode(text),
+                      text
+                    }
+                  }
+
+                  return output
+                }))
+              }
+            )
+          }
+        }
+      ]
+    }
+  ],
   sourcemap: true,
   sourcesContent: false,
   target: 'node' + pkg.engines.node.replace(/^\D+/, ''),
